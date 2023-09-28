59223e69fe9ed81bbfcc6755d229ebb4f42f6ede
Rachid amhouch <rachid.amhouch@gmail.com>
2023-09-28 23:49:09 +0100

    anything

--- routes/home.js
+++ routes/home.js
@@ -4,11 +4,29 @@ import axios from "axios";
 const router = express.Router()
 
 router.get("/", async (req, res) => {
-    res.render("home.ejs")
+    const ip = await axios.get("https://api.ipify.org?format=json")
+    const loction = await axios.get(`https://ipinfo.io/${ip.data.ip}/geo`)
+    const temp = await axios.get(`https://api.weatherapi.com/v1/current.json?key=a577836ded914bb7bb5155450230109&q=${loction.data.city}`)
+    const data = {
+        data: temp.data,
+        region: loction.data.region
+    }
+    res.render("home.ejs", data)
 })
 
 router.post("/", async (req, res) => {
-    res.render("home.ejs")
+    try{
+        const temp = await axios.get(`https://api.weatherapi.com/v1/current.json?key=a577836ded914bb7bb5155450230109&q=${req.body["city"]}`)
+        const data = {
+            data: temp.data
+        }
+        res.render("home.ejs", data)
+    }catch(error)
+    {
+        res.render("home.ejs", {
+            error: "No matching location found"
+        })
+    }
 })
 
 export default router
